b35a015013d22db8608451b1a91f0e31cf1a9feb
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-06-05 01:06:06 +0500

    Bump the version to 2.0.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ def package_data(pkg, root_list):
 
 setup(
     name='recommender-xblock',
-    version='2.0.0',
+    version='2.0.1',
     description='recommender XBlock',   # TODO: write a better description.
     long_description=README,
     author='edX',
