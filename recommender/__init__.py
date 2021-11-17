6542af325be5ac9c5ff1878cb0933083fc12343e
Mohammad Ahtasham ul Hassan <ahthassan74@gmail.com>
2021-11-17 15:56:19 +0500

    build: standardize version number placement

--- recommender/__init__.py
+++ recommender/__init__.py
@@ -3,3 +3,5 @@ This XBlock will show a set of recommended resources which may be helpful to
 students solving a given problem.
 """
 from .recommender import RecommenderXBlock
+
+__version__ = '2.1.0'
